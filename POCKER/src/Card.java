d35c3537de2fccdfcf48398d65774b30710a1183
Tiến Tài <tientai206@gmail.com>
2018-01-23 06:56:28 +0700

    v1

--- POCKER/src/Card.java
+++ POCKER/src/Card.java
@@ -1,27 +1,7 @@
 
 public class Card {
-	private short rank, suit;
 	public  String[] ranks = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
 	public  char[] suits = {'C','D','H','S'};
-
-
-	public short getRank() {
-		return rank;
-	}
-
-	public void setRank(short rank) {
-		this.rank = rank;
-	}
-
-	public short getSuit() {
-		return suit;
-	}
-
-	public void setSuit(short suit) {
-		this.suit = suit;
-	}
-	
-	
 }
 	
 	
